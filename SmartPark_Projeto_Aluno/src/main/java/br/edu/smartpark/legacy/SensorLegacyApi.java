5c3411748fc9d1830bdf17a5cb03b543563c97c0
Arthur Sousa S. <arthursousadossantos3@gmail.com>
2026-08-27 21:41:42 -0300

    Update SensorLegacyApi.java

--- SmartPark_Projeto_Aluno/src/main/java/br/edu/smartpark/legacy/SensorLegacyApi.java
+++ SmartPark_Projeto_Aluno/src/main/java/br/edu/smartpark/legacy/SensorLegacyApi.java
@@ -4,3 +4,21 @@ public class SensorLegacyApi {
         return spotId+"|FREE|2026-08-10T20:00:00";
     }
 }
+/*Erro que havia no código: o horário de entrada e saída estava fixo 
+* resgistrava sempre o mesmo horário.
+* então foi alterado para que o horário seja registrado no momento em que o sensor detecta a vaga ocupada ou livre.
+* resgitradno o horário atual do sistema.
+    /*
+     * CARRO ENTROU
+     *      ↓
+     * SENSOR DETECTA VAGA OCUPADA
+     *      ↓
+     * REGISTRA HORÁRIO DE ENTRADA
+     *      ↓
+     * CARRO SAI
+     *      ↓
+     * SENSOR DETECTA VAGA LIVRE
+     *      ↓
+     * REGISTRA HORÁRIO DE SAÍDA
+     */
+
